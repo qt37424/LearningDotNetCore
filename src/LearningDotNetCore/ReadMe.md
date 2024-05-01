e9074f65080ddf61e7fb023c4de32954dd8de6ea
qt37424 <qt37424@gmail.com>
2024-05-01 16:19:53 +0700

    Add model for project
    Note: do not know in web service need declare constructor?

---
+++ src/LearningDotNetCore/ReadMe.md
@@ -0,0 +1,4 @@
+### NOTE cho project COUPONAPI
+1. Có thể thay đổi port mạng https hoặc http bằng cách sửa trong file config
+
+2. Có thể đổi màu tab của trên visual studio theo màu của từng project riêng lẻ
---
+++ src/LearningDotNetCore/Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,35 @@
+﻿namespace Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        #region [Properties]
+
+        /// <summary>
+        /// CouponID
+        /// </summary>
+        /// Nghien cuu xem co cach nao change coupon id thanh string de add uuid duoc khong? Hoac don gian la de add uuid
+        public int CouponId { get; set; }
+
+        /// <summary>
+        /// Coupon code
+        /// </summary>
+        public string CouponCode { get; set; }
+
+        /// <summary>
+        /// Discount Amount
+        /// </summary>
+        public double DiscountAmount { get; set; }
+
+        /// <summary>
+        /// Min Amount value
+        /// </summary>
+        public int MinAmount { get; set; }
+
+        /// <summary>
+        /// Last Updated
+        /// </summary>
+        public DateTime LastUpdated { get; set; }
+
+        #endregion
+    }
+}
